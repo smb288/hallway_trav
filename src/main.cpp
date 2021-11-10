ae8c6f124f4c869f49120933b5b39173e1725141
smb288 <sbaker101@gmail.com>
2021-11-10 14:49:07 -0600

    Sumo ring detection

--- src/main.cpp
+++ src/main.cpp
@@ -6,6 +6,8 @@
 #define trigPin2 3
 #define echoPin3 4
 #define trigPin3 5
+#define IR1 6
+#define IR2 7
 #define Motor1 16
 #define Motor2 17
 #define Motor3 18
@@ -17,13 +19,19 @@ long duration; // variable for the duration of sound wave travel
 int distanceM, distanceL, distanceR; // variable for the distance measurement
 int m1Speed = 250;
 int m2Speed = 255;
-
+int m1Slow = 100;
+int m2Slow = 100;
+bool FOUNDMID = false;
+bool FOUNDLEFT = false;
+bool FOUNDRIGHT = false;
 void setup() {
   pinMode(trigPin1, OUTPUT); // Sets the trigPin as an OUTPUT
   pinMode(echoPin1, INPUT); // Sets the echoPin as an INPUT
   pinMode(trigPin2, OUTPUT); // Sets the trigPin as an OUTPUT
   pinMode(echoPin2, INPUT);
   pinMode(trigPin3, OUTPUT); // Sets the trigPin as an OUTPUT
+  pinMode(IR1, INPUT);
+  pinMode(IR2, INPUT);
   pinMode(echoPin3, INPUT);
   pinMode(Motor1, OUTPUT);
   pinMode(Motor2, OUTPUT);
@@ -47,9 +55,63 @@ int pulseUltra(int echo, int trig) {
   
   return distance;
 }
+
+//movement functions to move the car
+void forward(){
+  analogWrite(Motor1, m1Speed);
+  analogWrite(Motor2, 0);
+  analogWrite(Motor3, 0);
+  analogWrite(Motor4, m2Speed);
+}
+
+void back(){
+  analogWrite(Motor1, 0);
+  analogWrite(Motor2, m1Speed);
+  analogWrite(Motor3, m2Speed);
+  analogWrite(Motor4, 0);
+}
+
+void right(){
+  analogWrite(Motor1, m1Slow);
+  analogWrite(Motor2, 0);
+  analogWrite(Motor3, 0);
+  analogWrite(Motor4, 0);
+}
+
+void left(){
+  analogWrite(Motor1, 0);
+  analogWrite(Motor2, 0);
+  analogWrite(Motor3, 0);
+  analogWrite(Motor4, m2Slow);
+}
+
+void backright(){
+  analogWrite(Motor1, 0);
+  analogWrite(Motor2, m1Speed);
+  analogWrite(Motor3, m2Slow);
+  analogWrite(Motor4, 0);
+}
+
+void backleft(){
+  analogWrite(Motor1, 0);
+  analogWrite(Motor2, m1Slow);
+  analogWrite(Motor3, m2Speed);
+  analogWrite(Motor4, 0);
+}
+
+void stop(){
+  back();
+  delay(10);
+  analogWrite(Motor1, 0);
+  analogWrite(Motor2, 0);
+  analogWrite(Motor3, 0);
+  analogWrite(Motor4, 0);
+}
+
+//-------------------------------------------------------------------------------------------
+//Main Loop function with algorithm
+//-------------------------------------------------------------------------------------------
 void loop() {
-  int motor1Speed = map(m1Speed, 0, 1023, 0, 255);
-  int motor2Speed = map(m2Speed, 0, 1023, 0, 255);
   distanceM = pulseUltra(echoPin1, trigPin1);
   distanceL = pulseUltra(echoPin2, trigPin2);
   distanceR = pulseUltra(echoPin3, trigPin3);
@@ -63,25 +125,36 @@ void loop() {
   Serial.print("\tDistanceR: ");
   Serial.print(distanceR);
   Serial.println(" cm");*/
-  if(distanceM > 30) {
-    analogWrite(Motor1, motor1Speed);
-    analogWrite(Motor2, 0);
-    analogWrite(Motor3, 0);
-    analogWrite(Motor4, motor2Speed);
+
+  if(digitalRead(IR1) && digitalRead(IR2)) {
+    if(distanceM < 45){
+      forward();
+      delay(100);
+    }
+    else if(distanceR < 45){
+      right();
+      delay(100);
+    }
+    else if(distanceL < 45){
+      left();
+      delay(100);
+    }
+    else{
+      right();
+      delay(100);
+    }
   }
-  else if(distanceL > distanceR) {
-    analogWrite(Motor1, 0);
-    analogWrite(Motor2, 0);
-    analogWrite(Motor3, 0);
-    analogWrite(Motor4, motor1Speed);
-    delay(100);
+  else if (!digitalRead(IR1)){
+    back();
+    delay(500);
+    right();
+    delay(500);
   }
-  else if(distanceL < distanceR) {
-    analogWrite(Motor1, motor1Speed);
-    analogWrite(Motor2, 0);
-    analogWrite(Motor3, 0);
-    analogWrite(Motor4, 0);
-    delay(100);
+  else if (!digitalRead(IR2)){
+    forward();
+    delay(500);
+    left();
+    delay(500);
   }
   delay(1);
 }
